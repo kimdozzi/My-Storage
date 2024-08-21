60a9c43caa8e169028b1c35bdcf7d7eecb111718
kimdozzi <neo5188@gmail.com>
2024-08-21 17:18:51 +0900

    first commit

---
+++ src/main/java/com/example/mystorage/MystorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mystorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mystorage/MystorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
